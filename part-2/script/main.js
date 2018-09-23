3c5c1ad133627a27123fa166252812c56ca348f5
Felipe Hernandez <felipeneo@hotmail.com>
2018-09-23 14:59:18 -0400

    add process data and DOM manipulation for images

--- part-2/script/main.js
+++ part-2/script/main.js
@@ -15,18 +15,20 @@ const populateDOM = function() {
 }
 
 // MANIPULATE DOM - ADD DATA TO DOM
-const manipulateDom = function(index,title,description){
+const manipulateDom = function(imageUrl,title,description){
   let moduleX = `#module${index}`;
+  $(`${moduleX} .image`).css('background-image', `url(${imageUrl})`);
   $(`${moduleX} .grid-heading`).text(title);
   $(`${moduleX} .grid-text`).text(description);
 }
 
 // PROCESS THE DATA COMING FROM API
 const processData = function(){
+  let imageUrl = allData.data[2].mediaurl;
   let title = allData.data[index].title;
   let description = allData.data[index].description;
 
-  manipulateDom(index,title,description);
+  manipulateDom(imageUrl,title,description);
 }
 
 // MAKE AJAX CALL TO PROVIDED API
