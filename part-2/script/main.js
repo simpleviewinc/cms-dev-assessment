6b4450585becbf79f2517f7ee852318761cbd0d0
Felipe Hernandez <felipeneo@hotmail.com>
2018-09-23 13:08:16 -0400

    add ajax function call

--- part-2/script/main.js
+++ part-2/script/main.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
   console.log('main.js is connected!');
+  // FUNCTION FOR AJAX API CALL
+  makeCall();
 });
+
+const makeCall = function(){
+  // MAKE AJAX CALL TO PROVIDED API
+  $.ajax({
+    url: 'https://sv-reqres.now.sh/api/listings/?per_page=2',
+    type: "Get",
+    dataType: 'json'
+  }).then(function(response){
+    console.log(response);
+  })
+}
