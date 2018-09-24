56ae545eded03525d43e03b820e4f9cfcff0f14e
Felipe Hernandez <felipeneo@hotmail.com>
2018-09-23 22:23:47 -0400

    add ajax function to request data from provided api

--- part-3/script/main.js
+++ part-3/script/main.js
@@ -6,7 +6,6 @@ $(document).ready(function() {
 $('.nav-item').click(function(){
   $('.nav-item').removeClass('active');
   $(this).addClass('active');
-
 });
 
 // TOGGLE ACTIVE CLASS NAVIGATION
@@ -14,3 +13,14 @@ $('.nav-bttn').click(function(){
   $('.nav-bttn').removeClass('active');
   $(this).addClass('active');
 });
+
+// MAKE AJAX CALL TO PROVIDED API
+const makeCall = function(eventType){
+  $.ajax({
+    url: `https://sv-reqres.now.sh/api/${eventType}/?per_page=2`,
+    type: "Get",
+    dataType: 'json'
+  }).then(function(response){
+    console.log(response);
+  })
+}
