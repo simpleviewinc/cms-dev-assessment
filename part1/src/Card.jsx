3f55fd18f2301c5333cd06f87878bd4941793a35
soldierforus <tbennett3434@gmail.com>
2019-02-05 20:51:06 -0700

    cards and images centered and justified with buttons

---
+++ part1/src/Card.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Card.css'
+
+const Card = () => {
+  return (
+    <div className="card">
+      <span className="card-header" />
+      <h3>Lorem ipsum dolor sit</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua. Quis hendrerit dolor magna eget est. Risus feugiat in ante
+        metus dictum. Imperdiet proin fermentum leo vel orci. Enim eu turpis egestas pretium aenean
+        pharetra magna. Nulla pellentesque dignissim enim sit amet. Turpis cursus in hac habitasse.
+        Vitae suscipit tellus mauris a. Sed blandit libero volutpat sed cras ornare arcu dui. Lorem
+        dolor sed viverra ipsum nunc aliquet.
+        <button>Read More</button>
+      </p>
+    </div>
+  )
+}
+
+export default Card
---
+++ part1/src/Grid.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import './Grid.css'
+
+import Card from './Card'
+
+class Grid extends Component {
+  render() {
+    return (
+      <div className="Grid">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    )
+  }
+}
+
+export default Grid
