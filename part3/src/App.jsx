1ee1ab51994efa590d6974a81f21a646fa12fe53
soldierforus <tbennett3434@gmail.com>
2019-02-07 18:34:57 -0700

    removed nav component put buttons in grid for easier API calls

--- part3/src/App.jsx
+++ part3/src/App.jsx
@@ -1,13 +1,11 @@
 import React, { Component } from 'react'
 import './App.css'
 import Grid from './Grid'
-import Nav from './Nav'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Nav />
         <Grid />
       </div>
     )
--- part3/src/Grid.jsx
+++ part3/src/Grid.jsx
@@ -7,6 +7,23 @@ class Grid extends Component {
     data: [],
   }
 
+  // filter functions for buttons
+  showAll() {
+    console.log('All')
+  }
+
+  showListings() {
+    console.log('Listings')
+  }
+
+  showEvents() {
+    console.log('Events')
+  }
+
+  showOffers() {
+    console.log('Offers')
+  }
+
   // Code is invoked after the component is mounted/inserted into the DOM tree.
   componentDidMount() {
     const url = 'https://sv-reqres.now.sh/api/listings/'
@@ -14,7 +31,6 @@ class Grid extends Component {
     fetch(url)
       .then(result => result.json())
       .then(result => {
-        console.log(JSON.stringify(result, null, 4))
         this.setState({
           data: result.data,
         })
@@ -26,11 +42,27 @@ class Grid extends Component {
   render() {
     const { data } = this.state
     return (
-      <div className="Grid">
-        {/* // item is the item of 'data' array */}
-        {data.map(item => {
-          return <Card title={item.title} description={item.description} img={item.mediaurl} />
-        })}
+      <div>
+        <div className="nav">
+          <button type="button" onClick={this.showAll}>
+            All
+          </button>
+          <button type="button" onClick={this.showListings}>
+            Listings
+          </button>
+          <button type="button" onClick={this.showEvents}>
+            Events
+          </button>
+          <button type="button" onClick={this.showOffers}>
+            Offers
+          </button>
+        </div>
+        <div className="Grid">
+          {/* // item is the item of 'data' array */}
+          {data.map(item => {
+            return <Card title={item.title} description={item.description} img={item.mediaurl} />
+          })}
+        </div>
       </div>
     )
   }
