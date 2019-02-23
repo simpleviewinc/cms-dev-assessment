63904758f38b24d96ad3eec18a849c0d4cec69e4
Jacob Leon Lyerla <umasour@yahoo.com>
2019-02-22 21:36:29 -0700

    Router and router testing added
    added in buttons that I could use to link to my Listings component route, This allowed me to test that my program was routing correctly.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import { Button } from "reactstrap";
+import "./App.css";
+import { Route, Link } from "react-router-dom";
+
+import Listings from "./comps/part 1-2/listings"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-     test
-          </div>
+        <Link to="/listings">
+          <Button color="primary">Part 1-2</Button>
+        </Link>
+        <Link to="/offers"> <Button color="warning">Part 3-4</Button></Link>
+
+        <Route exact path="/listings" render={() => <Listings />} />
+      </div>
     );
   }
 }
