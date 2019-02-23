1008b4b6a64b79f152e646df7ecc2ee2dd30e081
Jacob Leon Lyerla <umasour@yahoo.com>
2019-02-22 23:10:24 -0700

    used axios to request the data from the api
    use effect loads when the component loads, than it sets the data on state.

--- src/comps/contexts/listingsProvider.js
+++ src/comps/contexts/listingsProvider.js
@@ -2,31 +2,38 @@
 // I have never build a project around hooks before,
 // so the syntax maybe a little off.
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 export const ListingsContext = React.createContext();
 
-const ListingsProvider = (props) => {
-  const listingsInformation = {
-    userName: 'test name',
+const ListingsProvider = props => {
+  useEffect(() => {
  
+    axios
+    .get("https://sv-reqres.now.sh/api/listings/?per_page=2")
+  .then(response =>{
+  listingsInformation.setListings("listings",response.data)
+  })
+  });
+  const listingsInformation = {
+    userName: "test name",
+    listings:[],
+
     setListings: (property, value) => {
       setListings({
         ...listingsInfo,
         [property]: value
       });
-    },
-    hello: ()=>{
-     console.log("hello")
- }
-  }
- 
+    }
+  };
+
   // create `userInfo` with update method called `setUserInfo`
   // set default to `userInformation`
   const [listingsInfo, setListings] = useState(listingsInformation);
   return (
-    <ListingsContext.Provider value={{listingsInfo}}>
+    <ListingsContext.Provider value={{ listingsInfo }}>
       {props.children}
     </ListingsContext.Provider>
-  )
-}
-export default ListingsProvider
+  );
+};
+export default ListingsProvider;
