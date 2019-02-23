6d3cc3b7baab294b1d837003e4476568c1a6f195
Jacob Leon Lyerla <umasour@yahoo.com>
2019-02-22 22:11:35 -0700

    provided context to my route component
    I imported in my provider and context than wrapped and passed the variable into my route component, inside of a render prop.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { Button } from "reactstrap";
+import ListingsProvider, { ListingsContext } from "./comps/contexts/listingsProvider";
+
 import "./App.css";
 import { Route, Link } from "react-router-dom";
 
@@ -13,8 +15,13 @@ class App extends Component {
           <Button color="primary">Part 1-2</Button>
         </Link>
         <Link to="/offers"> <Button color="warning">Part 3-4</Button></Link>
-
-        <Route exact path="/listings" render={() => <Listings />} />
+        <ListingsProvider><ListingsContext.Consumer>
+          {context =>(
+             <Route exact path="/listings" render={() => <Listings context={context} />} />
+          )}
+          
+          </ListingsContext.Consumer></ListingsProvider>
+       
       </div>
     );
   }
