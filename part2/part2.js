61054749199aa61a827a8ddbdccfe81e58c4dfbc
Laurel Thomson <laurel.beth.thomson@gmail.com>
2019-07-24 22:57:39 -0500

    Changing the filtering to be client-side for part 2

--- part2/part2.js
+++ part2/part2.js
@@ -2,12 +2,12 @@ Vue.component('part1', {
     template: `
     <div id="listings" class="grid_container">
         <div class="header">
-            <button type="button" v-on:click="getData($event,'all')">All</button>
-            <button type="button" v-on:click="getData($event,'listings')">Listings</button>
-            <button type="button" v-on:click="getData($event,'events')">Events</button>
-            <button type="button" v-on:click="getData($event,'offers')">Offers</button>
+            <button type="button" v-on:click="filter($event,'all')">All</button>
+            <button type="button" v-on:click="filter($event,'listings')">Listings</button>
+            <button type="button" v-on:click="filter($event,'events')">Events</button>
+            <button type="button" v-on:click="filter($event,'offers')">Offers</button>
         </div>
-        <div v-for="(listing, index) in listings" class="listing_wrapper"
+        <div v-for="(listing, index) in visible" class="listing_wrapper"
         :class="[ index % 6 == 0 || index % 6 == 5 ? (index % 6 == 0 ? 'listing_item_big1' : 'listing_item_big2') : 'listing_itemx_small']">
             <img class="thumbnail" :src="listings.mediaurl || '../comps/fallback.jpg'">
             <div class="listing_item">
@@ -20,19 +20,48 @@ Vue.component('part1', {
     `,
     data() {
         return {
-            listings: []
+            listings: [],
+            events: [],
+            offers: [],
+            all: [],
+            visible: []
+        }
+    },
+    methods: {
+        getListings: function() {
+            return axios.get('https://sv-reqres.now.sh/api/listings/?per_page=3')
+        },
+        getEvents: function() {
+            return axios.get('https://sv-reqres.now.sh/api/events/?per_page=3')
+        },
+        getOffers: function() {
+            return axios.get('https://sv-reqres.now.sh/api/offers/?per_page=3')
+        },
+        filter: function(event, display) {
+            switch(display) {
+                case 'all':
+                    this.visible = this.all;
+                    break;
+                case 'listings':
+                    this.visible = this.listings;
+                    break;
+                case 'events':
+                        this.visible = this.events;
+                        break;
+                case 'offers':
+                        this.visible = this.offers;
+                        break;
+            }
         }
     },
     mounted () {
-        axios
-        .get('https://sv-reqres.now.sh/api/listings/?per_page=15')
-        .then(response => (this.listings = response.data.data))
+        axios.all([this.getListings(), this.getEvents(),this.getOffers()])
+        .then(axios.spread((listings, events, offers) => {
+            this.listings = listings.data.data;
+            this.events = events.data.data;
+            this.offers = offers.data.data;
+            this.all = this.listings.concat(this.events).concat(this.offers);
+            this.visible = this.all;
+        }))
     },
-    methods: {
-        getData: function(event, type) {
-            axios
-            .get('https://sv-reqres.now.sh/api/' + type + '/?per_page=15')
-            .then(response => (this.listings = response.data.data))
-        }
-    }
 })
