4c2bf9603d84f513be98726f24d27904a75fe30d
Laurel Thomson <laurel.beth.thomson@gmail.com>
2019-07-25 07:58:26 -0500

    Setting the fallback image in part 1

--- part1/part1.js
+++ part1/part1.js
@@ -3,7 +3,7 @@ Vue.component('part1', {
     <div id="listings" class="grid_container">
         <div v-for="(listing, index) in listings" class="listing_wrapper"
         :class="[ index % 6 == 0 || index % 6 == 5 ? (index % 6 == 0 ? 'listing_item_big1' : 'listing_item_big2') : 'listing_itemx_small']">
-            <img class="thumbnail" :src="listings.mediaurl || '../comps/fallback.jpg'">
+            <img class="thumbnail" alt="Listing Image" :src="listing.mediaurl" @error="setFallbackImageUrl">
             <div class="listing_item">
                 <p><strong>{{listing.title}}</strong></p>
                 <p>{{ listing.description }}</p>
@@ -21,5 +21,10 @@ Vue.component('part1', {
         axios
         .get('https://sv-reqres.now.sh/api/listings/?per_page=15')
         .then(response => (this.listings = response.data.data))
+    },
+    methods: {
+        setFallbackImageUrl(event) {
+            event.target.src = "../comps/fallback.jpg"
+          }
     }
 })
