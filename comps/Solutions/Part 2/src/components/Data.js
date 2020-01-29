685e2761ddbdc3dad527b6e8020b6898b99d2149
Timothy McGowen <mcgowen17@yahoo.com>
2020-01-29 09:00:02 -0700

    Added comments and cleaned up some code on Part 2

--- comps/Solutions/Part 2/src/components/Data.js
+++ comps/Solutions/Part 2/src/components/Data.js
@@ -5,43 +5,46 @@ import InfoCard from './InfoCard';
 
 
 function Data() {
-    const [type, setType] = useState("listings") // Changes the data based on its state
-    const [data, setData] = useState([]); // Setting up state to recieve data
+    // Changes the data being called based on its state
+    const [type, setType] = useState("all")
+    // Setting up state to recieve data
+    const [data, setData] = useState([]);
+    // Used to trigger active style for the purple box
     const [active, setActive] = useState('All');
 
     useEffect(() => {
+        // Added a clear all of data state because of a bug that was letting the state stack up.
         setData([])
+        // Checks if the data you want to see is 'all', if it is it calls all three and saves them to variables
         if (type == 'all') {
-            const events = axios.get(`https://sv-reqres.now.sh/api/events`).then(function (response) {
-                return response.data.data;
-            });
-            const listings = axios.get(`https://sv-reqres.now.sh/api/listings`).then(function (response) {
-                return response.data.data;
-            });
-            const offers = axios.get(`https://sv-reqres.now.sh/api/offers`).then(function (response) {
-                return response.data.data;
-            });
+            const events = axios.get(`https://sv-reqres.now.sh/api/events`).then(e => { return e.data.data; });
+            const listings = axios.get(`https://sv-reqres.now.sh/api/listings`).then(e => { return e.data.data; });
+            const offers = axios.get(`https://sv-reqres.now.sh/api/offers`).then(e => { return e.data.data; });
 
+            // Resolves all three promises
             Promise.all([listings, events, offers]).then(eventPromises => {
-                var newArray = eventPromises.flat() // Takes the multi dimentional array and flattens it all into a useable array
+                // Takes the multi dimentional array and flattens it all into a useable array
+                var newArray = eventPromises.flat()
+                // Sets flattened array into data to be displayed
                 setData(newArray)
             })
                 .catch(err => {
-                    return "Unable to retrieve code inside Part 1 Data Component"
+                    return "Unable to retrieve code inside Part 1 Data Component";
                 })
         } else {
+            // If anything but 'all' the type is fed into the axios call and sets data into the correct section
             axios.get(`https://sv-reqres.now.sh/api/${type}`)
                 .then(e => {
                     setData(e.data.data)
                 })
                 .catch(err => {
-                    return "Unable to retrieve code inside Part 1 Data Component"
+                    return "Unable to retrieve code inside Part 2 Data Component"
                 })
         }
     }, [type])
 
+    // Created an onClick function that takes the value of the clicked button and sets type of call and which will be active
     const loadData = (event) => {
-        console.log("What is this ", event.target.value)
         switch (event.target.value) {
             case "All":
                 setType("all")
@@ -60,21 +63,24 @@ function Data() {
                 setActive("Offers")
                 break;
             default:
-                console.log("Broken in loadData function")
+                console.log("Error in loadData function")
         }
     }
 
-    // Display a picture, a Title, and Text. If the text outflows the box let it disappear. 
     return (
         <div>
-            <span className="buttons" id="current">
+            <span className="buttons">
+                {/* Settled with hard coding all four buttons and attaching the required event handlers  */}
                 <button className={active == 'All' ? 'active' : ''} onClick={loadData} value="All">All</button>
                 <button className={active == 'Listings' ? 'active' : ''} onClick={loadData} value="Listings">Listings</button>
                 <button className={active == "Events" ? 'active' : ''} onClick={loadData} value="Events">Events</button>
                 <button className={active == "Offers" ? 'active' : ''} onClick={loadData} value="Offers">Offers</button>
             </span>
             <div className="data">
+                {/* Using modulo operator I captured the remained of a repeating 6 pattern that resulted in 
+                3 unique className assignments, allowing me to do a repeating pattern of six.   */}
                 {data.map((value, index, key) => {
+                    {/* Mapping through array and display infoCard */ }
                     let remainder = (index + 1) % 6;
                     let classNam = "";
                     if (remainder === 1) classNam = 'topLeft';
--- comps/Solutions/Part 2/src/components/InfoCard.js
+++ comps/Solutions/Part 2/src/components/InfoCard.js
@@ -1,28 +1,25 @@
 import React, { useState } from "react";
-import fallBack from "../fallback.jpg"
+import fallBack from "../fallback.jpg"; // Imported image 
 
 function InfoCard(props) {
-
+    // Holds both the url sent from data, a fallback image, and an error check to be used in determining if a fallback image is needed
     const [imgLoader, setImgLoader] = useState({
         src: props.data.mediaurl,
         fallBackSrc: fallBack,
-        loaded: false,
         error: false
     })
-
+    // If onError is triggered then this function will run and set error to true, which will in turn 
+    // trigger the image assignment check.
     const onImageError = () => {
         setImgLoader({ ...imgLoader, error: true })
     }
 
-    const onImageLoaded = () => {
-        setImgLoader({ ...imgLoader, loaded: true })
-    }
-
+    // Checks if the image didn't render, if didn't it places fallBack image
     let srcImg = !imgLoader.error ? props.data.mediaurl : fallBack
 
     return (
         <div className={props.classNam}>
-            <img src={srcImg} alt={props.data.title} onLoad={onImageLoaded} onError={onImageError} />
+            <img src={srcImg} alt={props.data.title} onError={onImageError} />
             <section className="textFade">
                 <h2>{props.data.title}</h2>
                 <p>{props.data.description}</p>
