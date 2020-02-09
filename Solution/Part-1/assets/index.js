c6fc3cf7c89c58a52021e264855969f5630b3a50
Jesus Esquer <jme2791@gmail.com>
2020-02-09 08:34:27 -0700

    CSS grid for columns working on breakpoints

--- Solution/Part-1/assets/index.js
+++ Solution/Part-1/assets/index.js
@@ -5,24 +5,50 @@ $.ajax({
   url: queryURL,
   method: "GET"
 }).then(function(response) {
-  // GET array of results store in data
+  // GET array of results store in data variable
   let data = response.data;
-  // this method allow us to use the function generateCards() on each result
+  // This method allow us to use the function generateCards() on each result
   data.forEach(element => {
     generateCards(element.mediaurl, element.title, element.description);
+    addIndex()
   });
+
 });
 
-
 function generateCards(imageURL, title, description) {
-// We pass the data from GET method result and store it using an HTML string
-// Handle the onError event for the image to reassign its source using Jquery
-  let divCard = `<div class='card'>
-                <image class=image style='height:500px; width:500px;' src=${imageURL} 
+  // We pass the data from GET method result and store it in a variable as a HTML string
+  // Handle the onError event for the image to reassign its source using Jquery
+  let divCard = `<div class='card col-'>
+                <image class=image  src=${imageURL} alt=${title}
                 onerror="this.onerror=null;this.src='./assets/images/fallback.jpg';"></image>
                 <h2 class=header>${title}</h2>
                 <p class=description>${description}</p>
                 </div>`;
-  // After creating each card we append it to id cards              
+  // After creating each card we append it to 'id=cards'
   $("#cards").append(divCard);
 }
+
+function addIndex(){
+$(".card").each(function(index) {
+  if(index==0){
+  $(this).attr("dataset",index);
+  }
+});
+}
+
+012
+633
+345
+336
+
+678
+633
+
+911
+336
+
+111
+633
+
+111
+336
