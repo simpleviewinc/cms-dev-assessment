a52e82a9dc2650f7ccbd98f294e2427bc5830e2d
Jesus Esquer <jme2791@gmail.com>
2020-02-10 01:14:42 -0700

    Added toggle class function to cards div, getting elements from each page when changed

--- Solution/Part-3/assets/index.js
+++ Solution/Part-3/assets/index.js
@@ -1,7 +1,5 @@
-// On load run AJAX request
-document.onload =  sendAjaxRequest(),
-generateButtons(), handleOnClick();
- 
+// On load run
+document.onload = sendAjaxRequest();
 
 function generateCards(imageURL, title, description) {
   // We pass the data from GET method result and store it in a variable as a HTML string
@@ -26,37 +24,46 @@ function addCellIndex() {
 }
 
 function generateButtons() {
-  // Created buttons using JQuery added 'individual and btn' classes and data-field attribute for onClick 
-  let allBtn = $("<btn>All</btn>").addClass("allBtn btn");
+  // Created buttons using JQuery added 'individual and btn' classes and data-field attribute for onClick
+  let allBtn = $("<btn>All</btn>")
+    .addClass("allBtn btn")
+    .attr("data-field", "listings");
   let listingsBtn = $("<button>Listings</button>")
     .addClass("listingsBtn btn")
     .attr("data-field", "listings");
-  let eventsBtn = $("<button>Events</button>").addClass("eventsBtn btn").attr("data-field", "events");
-  let offersBtn = $("<button>Offers</button>").addClass("offersBtn btn").attr("data-field", "offers");
+  let eventsBtn = $("<button>Events</button>")
+    .addClass("eventsBtn btn")
+    .attr("data-field", "events");
+  let offersBtn = $("<button>Offers</button>")
+    .addClass("offersBtn btn")
+    .attr("data-field", "offers");
   // Append buttons to filtes-btns div
   $("#filter-btns").append(allBtn, listingsBtn, eventsBtn, offersBtn);
 }
-function handleOnClick(){
+function handleOnClick() {
   // Saved click button event in function
-$(".btn").click(function(event) {
-  event.preventDefault();
-  // Clear cards div to replace each time clicked
-  $('#cards').empty()
-  // Get data-field from button when clicked
-  query = $(this).attr("data-field");
-  // Run AJAX requeste passing data field as parameter
-  sendAjaxRequest(query);
-});
+  $(".btn").click(function(event) {
+    event.preventDefault();
+    // Clear cards div to replace each time clicked
+    $("#cards").empty();
+    // Get data-field from button when clicked
+    query = $(this).attr("data-field");
+    // Run AJAX requeste passing data field as parameter
+    sendAjaxRequest(query, 1, 12);
+    toggleCardClass("grid");
+  });
 }
 
-function sendAjaxRequest(query) {
+function sendAjaxRequest(query, page, elements) {
   // Saved AJAX GET request in function
   // If no query use listings as default
-  if(query==null){
-    query = 'listings'
+  if (query == null && page == null && elements == null) {
+    query = "listings";
+    page = 1;
+    elements = 12;
   }
   // Saved query in variable passed query parameter
-  let queryURL = `https://sv-reqres.now.sh/api/${query}/?per_page=4`;
+  let queryURL = `https://sv-reqres.now.sh/api/${query}/?page${page}&per_page=${elements}`;
   // AJAX call with GET method
   $.ajax({
     url: queryURL,
@@ -73,16 +80,43 @@ function sendAjaxRequest(query) {
 }
 
 function generatePagesButtons() {
-  // Created buttons using JQuery added 'individual and btn' classes and data-field attribute for onClick 
+  // Created buttons using JQuery added 'individual and btn' classes and data-field attribute for onClick
   let pagesBtns = `<ul>
-  <li><a href="#"><<</a></li>
-  <li><a href="#" class="active">1</a></li>
-  <li><a href="#">2</a></li>
-  <li><a href="#">3</a></li>
-  <li><a href="#">4</a></li>
-  <li><a href="#">>></a></li>
-</ul>`
+  <li><a href="#" rel="previous"><<</a></li>
+  <li><a href="#" data-field="5" page="1" id='one' class="active page">1</a></li>
+  <li><a href="#" data-field="5" page="2" id='two' class="page">2</a></li>
+  <li><a href="#" data-field="3" page="3" id='three' class="page">3</a></li>
+  <li><a href="#" data-field="2" page="4" id='four' class="page">4</a></li>
+  <li><a href="#" rel="next">>></a></li>
+</ul>`;
   // Append buttons to pages-btns div
   $("#pages-btns").append(pagesBtns);
 }
+
+function handlePageChange() {
+  // Saved click button event in function
+  $(".page").click(function(event) {
+    event.preventDefault();
+    // Clear cards div to replace each time clicked
+    $("#cards").empty();
+    // Get data-field from button when clicked
+    let page = $(this).attr("page");
+    let elements = $(this).attr("data-field");
+    let name =  $(this).attr("id");
+    toggleCardClass(name);
+    console.log(page, elements);
+    // Run AJAX requeste passing data field as parameter
+    sendAjaxRequest("listings", page, elements);
+  });
+}
+
+function toggleCardClass(name) {
+  // Give each card a cell+index+1 class to start from 1
+  $("#cards").removeClass();
+  $("#cards").addClass(name);
+}
+
+generateButtons();
+handleOnClick();
 generatePagesButtons();
+handlePageChange();
