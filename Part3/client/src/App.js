cfdf16e1a27852bcdb6f31f52e619844a10a4b2e
Devon <Dvowen@cox.net>
2020-02-21 12:53:54 -0800

    finished section 3

---
+++ Part3/client/src/App.js
@@ -0,0 +1,127 @@
+import React, {Component} from 'react';
+import './App.css';
+import ListingBox from './Components/listingBox'
+import Axios from 'axios'
+
+class App extends Component{
+
+  constructor(props) {
+    super(props);
+    
+    this.state={
+
+      data: [],
+      focused: 'All',
+      show: false,
+      pagination: 0
+
+
+    }
+  }
+  
+componentDidMount(){
+  this.getAll()
+  this.animatedCover()
+
+}
+
+animatedCover = () => {
+  setTimeout(()=>this.setState({show: false}), 100)
+  setTimeout(()=>this.setState({show: true}), 1200)
+}
+
+paginationAdd = () => {
+  if(this.state.pagination === 3){
+    this.setState({pagination: 0})
+  }
+  else{
+  this.setState({pagination: this.state.pagination + 1})
+  }
+}
+
+paginationMinus = () => {
+  if(this.state.pagination === 0){
+    this.setState({pagination: 0})
+  }
+  else{
+    
+    this.setState({pagination: this.state.pagination - 1})
+    }
+  
+}
+
+getAll = () => {
+  let num = 0
+  let data = []
+  Axios.get("https://sv-reqres.now.sh/api/listings")
+  .then((res)=>{
+    
+    
+    data = res.data.data
+    
+  })
+  .then(
+  Axios.get("https://sv-reqres.now.sh/api/events")
+  .then((res)=>{
+    
+    
+    data = data.concat(res.data.data)
+    
+  })  
+  ).then(
+  Axios.get("https://sv-reqres.now.sh/api/offers")
+  .then((res)=>{
+    
+    
+    data = data.concat(res.data.data)
+    
+  })
+  )
+  
+
+  setTimeout(()=>{
+    for(let i=0;i<data.length; i++){
+      data[i].number = num + 1
+      num++
+    }
+    console.log(data)
+    this.setState({data: data})
+  }, 500)
+}
+
+filter = (selected) => {
+  if(selected === 'All'){
+    this.getAll()
+  }
+  else {
+    Axios.get(`https://sv-reqres.now.sh/api/${selected.toLowerCase()}`)
+    .then((res)=>{
+      let num = 0
+      let data = []
+      data = res.data.data
+
+      setTimeout(()=>{
+        for(let i=0;i<data.length; i++){
+          data[i].number = num + 1
+          num++
+        }
+        console.log(data)
+        this.setState({data: data})
+      }, 100)
+    
+  })  
+  }
+
+  this.setState({focused: selected})
+}
+
+
+  render(){
+  return (
+    <div className="App">
+      <ListingBox paginationMinus={this.paginationMinus} paginationAdd={this.paginationAdd} animatedCover={this.animatedCover} filter={this.filter} state={this.state} />
+    </div>
+  );}
+}
+
+export default App;
---
+++ Part3/client/src/Components/big.jsx
@@ -0,0 +1,21 @@
+import React, {useState} from 'react'
+import backupImage from '../imgs/fallback.jpg'
+import LearnInfo from './LearnInfo'
+
+export default (props) => {
+
+    let [hidden, setHidden] = useState(true)
+   
+
+
+    return(
+        
+            <div className='bigCard' style={{backgroundImage: `url(${props.row.mediaurl}), url(${backupImage})`}} onMouseEnter={()=>setHidden(false)} onMouseLeave={()=>setHidden(true) }>
+                <div className={props.state.show === true ? 'coverHidden' : 'cover'}></div>
+                <div className='nameCard'><h2>{props.row.number}. {props.row.title}</h2></div>
+                
+                <LearnInfo info={props.row.description} hidden={hidden} />
+            
+        </div>
+    )
+}
