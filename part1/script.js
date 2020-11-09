ead1cea61f0cdeb888c7c710dba2b87896668537
Kaelyn Rieffer <kaelynrieffer@gmail.com>
2020-11-09 16:53:07 -0700

    seperated data into cards and gave class name

--- part1/script.js
+++ part1/script.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
     let listingUrl= "https://sv-reqres.now.sh/api/listings";
     
     function card (){
-
         $.ajax({
             url: listingUrl,
             method: 'GET'
@@ -12,19 +11,23 @@ $(document).ready(function(){
         });
 
         function createCard (item) {
-
+            let gridItem = document.createElement('div');
             let img = document.createElement('img');
             let title = document.createElement('h1');
             let text = document.createElement('p');
-    
+            
+            gridItem.setAttribute('class', 'grid-item'); 
             img.setAttribute('src', item.mediaurl);
             img.setAttribute('onError', 'this.onerror=null; this.src="./Assets/img/fallback.jpg";');            
             title.textContent = item.title;
             text.textContent = item.description;
 
-            $('.grid-container').append(img);
-            $('.grid-container').append(title);
-            $('.grid-container').append(text);
+            
+            $('.grid-container').append(gridItem);
+
+            gridItem.append(img);
+            gridItem.append(title);
+            gridItem.append(text);
 
         };
     
