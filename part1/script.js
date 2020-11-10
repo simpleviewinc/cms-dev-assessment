68aeac478fef3227272b78c6e958b6a279eb7a88
Kaelyn Rieffer <kaelynrieffer@gmail.com>
2020-11-09 17:36:24 -0700

    3 med col and 1 small col media query

--- part1/script.js
+++ part1/script.js
@@ -16,21 +16,18 @@ $(document).ready(function(){
             let title = document.createElement('h1');
             let text = document.createElement('p');
             
-            gridItem.setAttribute('class', 'grid-item'); 
+            gridItem.setAttribute('class', 'grid-item column'); 
             img.setAttribute('src', item.mediaurl);
             img.setAttribute('onError', 'this.onerror=null; this.src="./Assets/img/fallback.jpg";');            
             title.textContent = item.title;
             text.textContent = item.description;
 
-            
             $('.grid-container').append(gridItem);
 
             gridItem.append(img);
             gridItem.append(title);
             gridItem.append(text);
-
         };
-    
     };
     card();
 });
