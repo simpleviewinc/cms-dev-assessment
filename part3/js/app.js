3a7bc7c8913321ad4b1511119a0358f7e671c0e6
Tyler Dickerson <23718942+tylerdickerson@users.noreply.github.com>
2021-01-20 08:10:36 -0700

    Part 3 async cleanup

--- part3/js/app.js
+++ part3/js/app.js
@@ -1,85 +1,102 @@
-(function() {
+const apiRoot = `https://sv-reqres.now.sh/api`;
+const itemsPerPage = `?per_page=20`;
+const pageLimits = [5, 5, 3, 2];
+let currentPage = 1;
 
-    const rootURL = 'https://sv-reqres.now.sh/api/';
-    const API = {
-        listings: `${rootURL}listings`,
-        events: `${rootURL}events`,
-        offers: `${rootURL}offers`
-    };
+async function getData(endpoint = "listings") {
+    const request = `${apiRoot}/${endpoint}/${itemsPerPage}`;
+    const response = await fetch(request);
+    const data = await response.json();
+    return data.data;
+}
 
-    let currentPage = 1;
-    const pageLimits = [5,5,3,2];
+async function getAllData() {
 
-    async function getData()
-    {
-        let markup = ``;
-        let itemNum = 0;
+    let allData;
+    try {
+        let data = await Promise.all([
+            getData(),
+            getData("events"),
+            getData("offers")
+        ]);
 
-            // API Request
-            const request = API.listings + `?per_page=15`;
-            const result = await $.get(request);
+        allData = [].concat(...data);
+    } catch (error) {
+        console.log(error)
+    }
+    return allData;
+}
 
-            for (let p = 0; p < pageLimits.length; p++)
-            {
-                markup += `<div class="flexGrid page${p + 1}">`;
+async function getAndDisplay() {
 
-                // Populate grid with max number of results for this page
-                for (let i = 0; i < pageLimits[p]; i++)
-                {
-                    let data = result.data[itemNum];
-                    itemNum++;
-                    markup += `
+    const data = await getData();
+    let markup = ``;
+    let itemNum = 0;
+
+    for (let p = 0; p < pageLimits.length; p++) {
+
+        // Create a parent grid with the page class set 
+        markup += `<div class="flexGrid page${p + 1}" hidden>`;
+
+        // Populate grid with max number of results for each page
+        for (let i = 0; i < pageLimits[p]; i++) {
+            
+            // Total count of items used for itemNum text
+            itemNum++;
+
+            let thisItem = data[itemNum - 1];
+            markup += `
                         <div class="item">
                             <div class="item_header">
                                 <span class="item_num">${itemNum.toString().length < 2 ? `0${itemNum}` : itemNum}.</span>
-                                <h2 class="item_title">${data.title}</h2>
+                                <h2 class="item_title">${thisItem.title}</h2>
                             </div>
                             <div class="item_imageContainer">
-                                <img class="item_image" src="${data.mediaurl}" alt="${data.title} image" onError="this.src = 'img/fallback.jpg';" />
+                                <img class="item_image" src="${thisItem.mediaurl}" alt="${thisItem.title} image" onError="this.src = 'img/fallback.jpg';" />
                             </div>
                         </div>`;
-                }
+        }
 
-                markup += `</div>`;
-            }
+        markup += `</div>`;
+    }
 
-        // Append HTML to the main container
-        $("#mainGrid").append(markup);
+    $("#mainGrid").html(markup);
+    $(".flexGrid").hide();
+    $('.page' + currentPage).show();
+}
 
-        // Hide all new grids once rendered
+function initEventListeners() {
+
+    $("#prev").click(function (e) {
+
+        // Hide all other grids
         $(".flexGrid").hide();
+
+        // If current page is 1, round-robin back to page 4, else nav to prev
+        currentPage === 1 ? currentPage = 4 : currentPage--;
+
+        // Show the new page
         $('.page' + currentPage).show();
 
-    }
+    });
 
-    function initEventListeners() {
-            
-        $("#prev").click(function(e){
+    $("#next").click(function (e) {
+        // Hide all other grids
+        $(".flexGrid").hide();
 
-            // Hide all other grids
-            $(".flexGrid").hide();
+        // If current page is 4, round-robin back to page 1, else nav to next
+        currentPage === 4 ? currentPage = 1 : currentPage++;
 
-            // If current page is 1, round-robin back to page 4, else nav to prev
-            currentPage === 1 ? currentPage = 4 : currentPage--;
+        // Show the new page
+        $('.page' + currentPage).show();
+    });
+}
 
-            // Show the new page
-            $('.page' + currentPage).show();
+function handleError(error)
+{
+    console.log(error)
+}
 
-        });
-
-        $("#next").click(function(e){
-            // Hide all other grids
-            $(".flexGrid").hide();
-
-            // If current page is 4, round-robin back to page 1, else nav to next
-            currentPage === 4 ? currentPage = 1 : currentPage++;
-
-            // Show the new page
-            $('.page' + currentPage).show();
-        });
-    }
-
-    // Initialize loading all
-    getData();
-    initEventListeners();
-})();
+// Get data and init event listeners
+getAndDisplay().catch(handleError);
+initEventListeners();
