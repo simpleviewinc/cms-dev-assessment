86534a97a2236d50888234d6ac49562a237bacb2
aleclivinghouse <aleclivinghouse@yahoo.com>
2021-02-04 21:47:09 -0800

    fetch all working

--- part-1/main2.js
+++ part-1/main2.js
@@ -4,18 +4,17 @@ function removeAllChildNodes(parent) {
     }
 }
 
-const createGrid = (data, unnapend) => {
-    if(document.querySelector(".container") && unnapend===true){
+const createGrid = (data) => {
+    if(document.querySelector(".container")){
         removeAllChildNodes(document.querySelector(".container"));
     }
-    const numOfRows = Math.ceil(data.length/3);
     let container = document.createElement('div');
     container.className = 'container';
  
     document.querySelector("#append").appendChild(container);
     let theRow;
+    let rowCount = 0;
     let boxCount = 1;
-    var rowCount = 0;
     var uniqueId = "index-1"
     var queryId = "#index-1" 
     data.map((item, index) => {
@@ -26,6 +25,7 @@ const createGrid = (data, unnapend) => {
             theRow =  document.createElement('div');
             theRow.className = 'row'
             theRow.id = "index-" + rowCount.toString();
+            queryId = "#index-" + rowCount.toString();
             document.querySelector(".container").appendChild(theRow);
             console.log("the rowCount is incrementing " + rowCount);
         }
@@ -72,18 +72,17 @@ const createGrid = (data, unnapend) => {
         }
         uniqueId= "index-" + rowCount.toString();
         queryId = "#index-" + rowCount.toString();
-        console.log("this is the query id ", queryId);
         document.querySelector(queryId).insertAdjacentHTML("beforeend", toInsert);
     })
 }
 
-async function fetchListings(unappend = true){
+async function fetchListings(){
     const response = await fetch("https://sv-reqres.now.sh/api/listings", {mode: 'cors'});
     const toJson = await response.json();
     const data = toJson.data;
     console.log("this is the data async", data);
     //we need to change this to get all
-    const toAppend = await createGrid(data, unappend);
+    const finished =  await createGrid(data);
 
 }
 
@@ -92,10 +91,78 @@ async function fetchEvents(){
     const toJson = await response.json();
     const data = toJson.data;
     console.log("this is the events", data);
-    const toAppend = await createGrid(data);
+    const finished =  await createGrid(data);
 
+}
+
+
+async function fetchOffers(){
+    const response = await fetch("https://sv-reqres.now.sh/api/offers", {mode: 'cors'});
+    const toJson = await response.json();
+    const data = toJson.data;
+    console.log("this is the events", data);
+    const finished = await createGrid(data);
+
+}
+
+async function fetchAll(){
+    const listingsResponse = await fetch("https://sv-reqres.now.sh/api/listings", {mode: 'cors'});
+    const listingsJSON = await listingsResponse.json();
+    const listingsData = listingsJSON.data;
+
+    const eventsResponse = await fetch("https://sv-reqres.now.sh/api/events", {mode: 'cors'});
+    const eventsJSON = await eventsResponse.json();
+    const eventsData = eventsJSON.data;
+
+    const offersResponse = await fetch("https://sv-reqres.now.sh/api/offers", {mode: 'cors'});
+    const offersJSON = await offersResponse.json();
+    const offersData = offersJSON.data;
+    const data = [...offersData, ...eventsData, ...listingsData];
+    console.log("this is all the data in fetchAll ", data);
+    const finished =  await createGrid(data);
 }
 const unnapend = true;
 fetchListings();
 
 console.log("loaded");
+
+
+function eventsTrigger(){
+    fetchEvents();
+}
+
+function listingsTrigger(){
+    fetchListings();
+}
+
+function offersTrigger(){
+    fetchOffers();
+}
+
+function allTrigger(){
+    fetchAll();
+}
+
+// document.addEventListener('click', function (event) {
+//     event.preventDefault();
+// 	// If the clicked element doesn't have the right selector, bail
+// 	if (event.target.matches(".listings")){
+//         fetchLists();
+//     } else if(event.target.matches(".events")){
+//         fetchEvents();
+//     } else if(event.target.matches(".offers")){
+//         fetchOffers();
+//     } else if(event.target.matches(".all")){
+//         fetchLists();
+//         unnappend=false;
+//         fetchEvents(unnappend);
+//         fetchOffers(unnappend);
+//     } else {
+//         return;
+//     }
+
+
+	// Log the clicked element in the console
+// 	console.log(event.target);
+
+// }, false);
