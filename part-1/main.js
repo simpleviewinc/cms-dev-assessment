c15beb826e9df34c118465aea3c9aa0f82072e32
aleclivinghouse <aleclivinghouse@yahoo.com>
2021-02-04 18:15:33 -0800

    three columns in each row

--- part-1/main.js
+++ part-1/main.js
@@ -7,21 +7,58 @@ const fetchListingsData = () => {
         console.log("this is the response in json data ");
         console.log(data.data);
 
+        let boxCount = 1;
+     
         const html = data.data.map((item, index) => {
-            return `
-                        <h1>${item.city}</h1>
-                        <p>${item.zip}</p>
-                        <img src="${item.mediaurl}" 
-                              height="400px" 
-                              width="400px" 
-                              onError="this.src = '../comps/fallback.jpg'"
-                              ></img>
-                    `;
+            let toInsert;
+            const descriptionStart = item.description.substring(0, 75);
+            const descriptionEnd = item.description.substring(75);
+            if(boxCount === 1) {
+                toInsert =  `
+                <div class="col-6">
+                <img src="${item.mediaurl}" 
+                      onError="this.src = '../comps/fallback.jpg'"
+                ></img>
+                <h1>${item.title}</h1>
+                <p>${descriptionStart}</p>
+                </div>
+            `;
+            } 
+            else if(boxCount === 6){
+                `
+                <div class="col-3">
+                    <img src="${item.mediaurl}" 
+                        onError="this.src = '../comps/fallback.jpg'"
+                        ></img>
+                </div>
+                <div class="col-3">
+                    <h1>${item.title}</h1>
+                    <p>${descriptionStart}</p>
+                 </div>
+            `;
+            } else {
+                toInsert =  `
+                <div class="col-3">
+                <img src="${item.mediaurl}" 
+                      onError="this.src = '../comps/fallback.jpg'"
+                ></img>
+                <h1>${item.title}</h1>
+                <p>${descriptionStart}</p>
+                </div>
+            `;
+            }
+            console.log("this is the boxCount ", boxCount);
+            if(boxCount === 6){
+                boxCount = 1;
+            } else {
+                boxCount++;
+            }
+            return toInsert;
         })
         .join("");
          console.log("this is the html", html);
 
-         document.querySelector("#test").insertAdjacentHTML("afterbegin", html);
+         document.querySelector(".row").insertAdjacentHTML("afterbegin", html);
     }).catch((err) => {
         console.log("this is the error ", err);
     });
---
+++ part-1/main2.js
@@ -0,0 +1,80 @@
+
+
+const createGrid = (data) => {
+    const numOfRows = Math.ceil(data.length/3);
+    let container = document.createElement('div');
+    container.className = 'container';
+    document.querySelector("#append").appendChild(container);
+    let theRow;
+    let boxCount = 1;
+    var rowCount = 0;
+    var uniqueId = "index-1"
+    var queryId = "#index-1" 
+    data.map((item, index) => {
+        const descriptionStart = item.description.substring(0, 75);
+        const descriptionEnd = item.description.substring(75);
+        if(index%3===0){
+            rowCount++;
+            theRow =  document.createElement('div');
+            theRow.className = 'row'
+            theRow.id = "index-" + rowCount.toString();
+            document.querySelector(".container").appendChild(theRow);
+            console.log("the rowCount is incrementing " + rowCount);
+        }
+
+        if(boxCount === 1) {
+            toInsert =  `
+            <div class="col-6">
+            <img src="${item.mediaurl}" 
+                  onError="this.src = '../comps/fallback.jpg'"
+            ></img>
+            <h1>${item.title}</h1>
+            <p>${descriptionStart}</p>
+            </div>
+        `;
+        } else if(boxCount === 6){
+               toInsert =  `
+                <div class="col-3">
+                    <img src="${item.mediaurl}" 
+                        onError="this.src = '../comps/fallback.jpg'"
+                        ></img>
+                </div>
+                <div class="col-3">
+                    <h1>${item.title}</h1>
+                    <p>${descriptionStart}</p>
+                 </div>
+            `;
+        } else {
+            toInsert =  `
+            <div class="col-3">
+            <img src="${item.mediaurl}" 
+                  onError="this.src = '../comps/fallback.jpg'"
+            ></img>
+            <h1>${item.title}</h1>
+            <p>${descriptionStart}</p>
+            </div>
+        `;
+        }
+        if(boxCount === 6){
+            boxCount = 1;
+        } else {
+            boxCount++;
+        }
+        uniqueId= "index-" + rowCount.toString();
+        queryId = "#index-" + rowCount.toString();
+        console.log("this is the query id ", queryId);
+        document.querySelector(queryId).insertAdjacentHTML("afterbegin", toInsert);
+    })
+}
+
+async function fetchApi(){
+    const response = await fetch("https://sv-reqres.now.sh/api/listings", {mode: 'cors'});
+    const toJson = await response.json();
+    const data = toJson.data;
+    console.log("this is the data async", data);
+    const toAppend = await createGrid(data);
+
+}
+
+fetchApi();
+console.log("loaded");
