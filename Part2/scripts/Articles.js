823bf1a0266536755e467e13a567ed5a6492350c
Schrese <53787633+Schrese@users.noreply.github.com>
2021-02-17 10:01:33 -0500

    Progress

---
+++ Part2/scripts/Articles.js
@@ -0,0 +1,206 @@
+const data = {"page":1,"per_page":6,"total":20,"total_pages":4,"data":[{"zip":"85719","address1":"303 University Blvd.","region":"East","description":"Located just north of Scottsdale in the scenic foothills of Carefree, Arizona, is the Carefree Resort and Villas. This 400+ room resort features spacious guest rooms, casitas and luxuriously appointed one-and two-bedroom vacation villas, all with private patios or balconies. In addition, the Resort offers more than 30,000 square feet of meeting and event space including the Grand Opera House, a highlight among the many unique meeting venues offered by the Resort. The Grand Opera House is ideal for catered events, meetings and convention groups, and features a combination Santa Fe-style architecture of Arizona's territorial days with the technical features of a modern studio sound stage. Carefree's very own western-themed venue, Stagecoach Pass, is located just outside the Opera House and provides a wonderful opportunity to experience the \"Old West\" in a historic frontier town setting. The Resort offers an abundance of recreation options including three sparkling pools, a full service spa, 5 tennis courts, all lit for night play, three restaurants and an authentic 1800's saloon with billiards, darts and big-screen TV. The Carefree Resort & Villas is partnered with the Legend Trail Golf Club and Golf Digest School only minutes from the Resort with golf packages and golf school vacations. and and and","city":"Tucson","state":"AZ","recid":313,"weburl":"http://www.starwoodhoteleast.com","phone":"520-897-7371","title":"Starwood Hotel East","category":{"subcatname":"Guest Ranch","catname":"Accommodations"},"mediaurl":"http://crm.rd.simpleviewinc.com/images/listings/original_Trident0.JPG","qualityScore":-4},{"zip":"85711","address1":"445 S. Alvernon Way","region":"Downtown","description":"In the heart of Scottsdale, within walking distance to great shopping, restaurants, entertainment, golf, Chaparral Suites Resort offers 311 two-room suites. Each suite has a private bedroom, spacious living room and includes two vanities, two remotecontrol TVs, microwave, refrigerator, coffee maker, iron/board, hair dryer, safe, voice messaging, complimentary in-room high-speed Internet, in-room movies. Play tennis, swim in two oversized pools, enjoy a whirlpool, or workout in our fitness center. Signature amenities include complimentary full cooked-to-order breakfast, evening reception, airport transportation, USA Today delivered to your room. Restaurant, Concierge Desk, car rental desk, gift shop, business center, laundry services.","city":"Tucson","state":"AZ","recid":360,"weburl":"http://www.thistlehoteldowntown.com","phone":"520-567-8582","title":"Thistle Hotel Downtown","category":{"subcatname":"Apparel","catname":"Shopping"},"mediaurl":"http://crm.rd.simpleviewinc.com/images/listings/thumb_simpleview-Logo15010.jpg","qualityScore":-4},{"zip":"85704","address1":"5910 N. Oracle Rd.","region":"West","description":"Full range of relocation services, including free area information packets, rental assistance, employment information, home finding and home sales assistance for individuals and corporations moving to or from Metro Phoenix.","city":"Tucson","state":"AZ","recid":419,"weburl":"http://www.towerhotelwest.com","phone":"520-403-8846","title":"Tower Hotel West","category":{"subcatname":"Automobile & RV Rentals","catname":"Transportation"},"mediaurl":"https://res.cloudinary.com/simpleview/image/upload/v1463070008/listings_default_tppucp.jpg","qualityScore":-4},{"zip":"85705-","address1":"1101 W. Prince Rd.","region":"South","description":"AAA approved, 2-Diamond property. All rooms with refrigerator, microwave, in-room coffee service, hair dryer, iron boards. New jacuzzi suites with fireplaces available. Free deluxe continental breakfast, modem dataports and WiFi, free shuttle to Mayo Clinic, golf packages, meeting room, outdoor pool/spa.","city":"Tucson","state":"AZ","recid":429,"weburl":"http://www.summerfieldsuitessouth.com","phone":"520-637-4523","title":"Summerfield Suites South","category":{"subcatname":"Bakeries & Delis","catname":"Dining"},"mediaurl":"https://res.cloudinary.com/simpleview/image/upload/v1463070008/listings_default_tppucp.jpg","qualityScore":-4},{"zip":"85706","address1":"7001 S. Tucson Blvd.","description":"This award winning Comfort Inn is located in the geographical middle of Scottsdale with very easy access to the Valley's loop freeway system. All rooms have coffeemakers and refrigerators and offer free high-speed Internet access. Wireless access is available in the lobby, breakfast area and on the patio. Included in the guestroom rate is a complimentary 24-item continental breakfast bar. USA Today newspaper, HBO and free local telephone calls. This conveniently located hotel also offers a large heated pool and whirlpool, as well as a complimentary exercise room and business center. Shuttle service is provided within a 5 mile radius of the hotel. Over 20 restaurants are located within walking distance of the hotel.","city":"Tucson","state":"AZ","recid":430,"weburl":"http://www.suttonplacehotelnorth.com","phone":"520-663-5324","title":"Sutton Place Hotel North","category":{"subcatname":"Biking","catname":"Attractions"},"mediaurl":"https://res.cloudinary.com/simpleview/image/upload/v1463070008/listings_default_tppucp.jpg","qualityScore":-4},{"zip":"85712","address1":"5118 E. Pima St.","description":"Has Yelp reviews associated with Howard Johnson in Tucson. Situated in the heart of Scottsdale and adjacent to Fashion Square Mall, the Days Inn offers Scottsdale style at Days Inn value. Clients call it one of the best kept secrets in Scottsdale. With over 80 restaurants and night clubs within walking distance, there's something to please everyone.","city":"Tucson","state":"AZ","recid":526,"weburl":"http://www.howardjohnson'snorth.com","phone":"520-622-5871","title":"Howard Johnson's North","category":{"subcatname":"Continental","catname":"Dining"},"mediaurl":"http://crm.rd.simpleviewinc.com/images/listings/Mario_Vasquez10.jpg","qualityScore":-4}]}
+
+const events = {"page":1,"per_page":6,"total":20,"total_pages":4,"data":[{"startDate":"2014-05-12T07:00:00.000Z","mediaurl":"http://crm.rd.simpleviewinc.com/images/calendar/09a36d6c-1f34-4ab8-b9da-53226b89d2b7.jpg","recurrence":"Recurring weekly on Monday, Tuesday, Wednesday","description":"A test Event dur","category":{"catname":"family Fun"},"title":"C Test Event","recid":"15"},{"startDate":"2014-10-25T07:00:00.000Z","mediaurl":"http://crm.rd.simpleviewinc.com/images/calendar/09a36d6c-1f34-4ab8-b9da-53226b89d2b7.jpg","recurrence":"Recurring every 3 weeks on Sunday, Saturday","description":"Marquee tag in the title","category":{"catname":"One item only event"},"title":"hello","recid":"42"},{"startDate":"2015-01-01T07:00:00.000Z","mediaurl":"http://crm.rd.simpleviewinc.com/images/calendar/7ad03e04-bb83-4698-8b8a-c626283f1c2c.jpg","recurrence":"Recurring every 3 weeks on Sunday, Saturday","description":"Bacon ipsum dolor sit amet beef ribs pork tenderloin, chicken cow meatloaf shoulder drumstick flank. Turducken short loin ground round meatball andouille venison. Swine corned beef turducken beef ribs meatball biltong pork tail. Ball tip pork belly landjaeger, chicken leberkas bresaola pastrami. Sausage ground round pork belly, andouille frankfurter shankle filet mignon. Brisket jerky tongue fatback t-bone meatloaf. Tongue fatback ham hock pancetta meatloaf turkey short loin pastrami andouille venison chicken. Shankle jowl turducken bresaola, chuck turkey filet mignon bacon spare ribs ribeye fatback pancetta tail shoulder tri-tip. Pork belly ham hock flank fatback, pork chop frankfurter pork loin beef sausage. Pork loin cow boudin, hamburger chicken filet mignon prosciutto. T-bone sirloin flank ball tip. Porchetta beef ball tip sausage ham hock meatloaf pancetta flank shankle pastrami spare ribs short ribs pork loin. Leberkas drumstick jowl pork loin. Meatball doner jowl, corned beef tenderloin kielbasa short loin andouille. Ribeye pork belly turducken, turkey hamburger leberkas capicola beef. Jowl short loin kevin, short ribs tenderloin salami shankle shank prosciutto pork chop shoulder filet mignon swine. Short ribs turkey jerky fatback jowl ham kielbasa bresaola strip steak meatball. Leberkas flank beef tail turducken, ground round jowl doner shankle ribeye filet mignon sirloin. Leberkas beef ribs biltong tri-tip andouille strip steak, corned beef porchetta ribeye bacon short ribs. Jowl doner tri-tip, bacon fatback chicken tongue jerky chuck cow. Ham hock flank shoulder ribeye, filet mignon pork chop ball tip. Sausage andouille salami kevin jerky, biltong rump capicola ground round. Ribeye pork chop flank, turkey meatloaf kevin biltong leberkas filet mignon jerky ham. Beef brisket boudin spare ribs, venison pig hamburger ribeye pork belly biltong frankfurter ham hock. Tenderloin corned beef meatball frankfurter ham rump boudin drumstick bresaola salami shoulder prosciutto pork belly. Landjaeger chuck jowl kevin.","category":{"catname":"family Fun"},"title":"National Day of Bacon","recid":"43"},{"startDate":"2016-05-19T07:00:00.000Z","mediaurl":"http://crm.rd.simpleviewinc.com/images/calendar/7ad03e04-bb83-4698-8b8a-c626283f1c2c.jpg","recurrence":"Recurring every 3 weeks on Sunday, Saturday","description":"Never expire test.  Never expire = Yes","category":{"catname":"pizza"},"title":"SKR Never Expire = true test","recid":"58"},{"startDate":"2014-05-08T07:00:00.000Z","mediaurl":"http://crm.rd.simpleviewinc.com/images/calendar/7ad03e04-bb83-4698-8b8a-c626283f1c2c.jpg","recurrence":"Recurring every 3 weeks on Sunday, Saturday","description":"this is a description","category":{"catname":"family Fun"},"title":"All fields with inputs","recid":"61"},{"startDate":"2014-05-08T07:00:00.000Z","mediaurl":"http://crm.rd.simpleviewinc.com/images/calendar/7ad03e04-bb83-4698-8b8a-c626283f1c2c.jpg","recurrence":"Recurring every 3 weeks on Sunday, Saturday","description":"this is a description","category":{"catname":"family Fun"},"title":"All fields with inputs","recid":"62"}]}
+
+const offers = {"page":1,"per_page":6,"total":20,"total_pages":4,"data":[{"title":"Testing Coupon with Grey Box","description":"offer text","redeemstart":"2009-07-23T07:00:00.000Z","category":{"catname":"BostonUSA Specials"},"recid":"8","mediaurl":"http://crm.rd.simpleviewinc.com/images/listings/original_Philadelphia0.JPG"},{"title":"Testing Coupon With Listing","description":"<br mce_bogus=\"1\">","redeemstart":"2009-07-23T07:00:00.000Z","category":{"catname":"BostonUSA Specials"},"recid":"74","mediaurl":"http://crm.rd.simpleviewinc.com/images/listings/original_Philadelphia0.JPG"},{"title":"What to do Coupon Kate","description":"This is the coupon offer text 8pt bold white text in green","redeemstart":"2009-07-23T07:00:00.000Z","category":{"catname":"What to Do"},"recid":"96","mediaurl":"http://crm.rd.simpleviewinc.com/images/listings/original_Philadelphia0.JPG"},{"title":"Kate's 2nd Coupon","description":"This is the 2nd coupon offer! in book antiqua pizza","redeemstart":"2009-07-23T07:00:00.000Z","category":{"categoryid":1,"catname":"BostonUSA Specials"},"recid":"97","mediaurl":"http://crm.rd.simpleviewinc.com/images/listings/original_Philadelphia0.JPG"},{"title":"Free Drinks with a purchase of Redesign","description":"Free adult bevarge with a purchase of a redesign","redeemstart":"2013-04-28T07:00:00.000Z","category":{"catname":"BostonUSA Specials"},"recid":"112","mediaurl":"http://crm.rd.simpleviewinc.com/images/listings/paradise0.jpg"},{"title":"Coupon #1","description":"Updated","category":{"catname":"BostonUSA Specials"},"recid":"120","mediaurl":"http://crm.rd.simpleviewinc.com/images/listings/original_img_1_Renderings-Amenity-Renderings-Overview_0000160.jpg"}]}
+
+// const fallback = "../../comps/fallback.jpg";
+const fallback = "../../fallback.jpg";
+const listings = document.querySelector('.articles-container');
+const navButtons = document.querySelector('.navbar-container');
+
+let allEntries = [];
+
+
+let listingsLink = "https://sv-reqres.now.sh/api/listings";
+let eventsLink = "https://sv-reqres.now.sh/api/events";
+let offersLink = "https://sv-reqres.now.sh/api/offers";
+
+const listingsGET = axios.get(listingsLink);
+const eventsGET = axios.get(eventsLink);
+const offersGET = axios.get(offersLink);
+
+function imageChecker(image, item) {
+    if (image.includes('res.cloudinary.com')) {
+        // console.log('good url')
+        listings.appendChild(createListings(item.title, item.description, item.mediaurl));
+    } else {
+        // console.log('bad url')
+        newImage = fallback;
+        listings.appendChild(createListings(item.title, item.description, newImage));        
+    }
+}
+
+const indArrayMapper = (list) => {
+        for (i = 0; i < list.length; i++) {
+            imageChecker(list[i].mediaurl, list[i]);
+            allEntries.push(list[i]);
+        }
+}
+
+
+    axios  
+        .all([listingsGET, eventsGET, offersGET])
+            .then(axios.spread((...responses) => {
+                const listingsRES = responses[0].data.data;
+                const eventsRES = responses[1].data.data;
+                const offersRES = responses[2].data.data;
+                
+                indArrayMapper(listingsRES);
+                indArrayMapper(eventsRES);
+                indArrayMapper(offersRES);
+
+            }))
+            .catch(err => {
+                console.log(err);
+            })
+
+// console.log(allEntries);
+
+let newList = []
+
+const filterer = async (keyword, list) => {
+    // console.log({key: keyword, li: list});
+    let list2 = []
+
+    if (keyword === 'all') {
+        x = document.getElementsByClassName('listing');
+        for (let i = 0; i < list.length; i++) {
+            imageChecker(list[i].mediaurl, list[i]);
+        }
+
+        // console.log(x);
+    }
+    else if (keyword === "listings") {
+        y = document.getElementsByClassName("listing");
+
+        for (let j = 6; j < list.length; j++) {
+            // console.log(j)
+            y[j].classList.add('hide-listing');
+        }
+        // console.log(x)
+
+    } else if (keyword === "events") {
+        z = document.getElementsByClassName("listing");
+
+        for (let k = 0; k < 6; k++) {
+            z[k].classList.add('hide-listing');
+        }
+
+        for (let k = 12; k < list.length; k++) {
+            z[k].classList.add('hide-listing');
+        }
+        // console.log(x)
+    } else if (keyword === "offers") {
+        z = document.getElementsByClassName("listing");
+        for (let k = 0; k < 12; k++) {
+            z[k].classList.add('hide-listing');
+        }
+    } else if (keyword === "clear") {
+        x = document.getElementsByClassName("listing");
+        
+        for (let i = 0; i < list.length; i++) {
+            x[i].classList.remove('hide-listing');
+        }
+    }
+    return newList
+}
+filterer("all", allEntries);
+
+const specialtyGetter = (arr) => {
+    arr.map(a => {
+        imageChecker(a.mediaurl, a)
+    })
+}
+
+
+function createNavButtons() {
+    const buttonsContainer = document.createElement('div');
+    const allButton = document.createElement('button');
+    const lButton = document.createElement('button');
+    const eButton = document.createElement('button');
+    const oButton = document.createElement('button');
+
+    // Sets up structure
+    navButtons.appendChild(buttonsContainer);
+    buttonsContainer.appendChild(allButton);
+    buttonsContainer.appendChild(lButton);
+    buttonsContainer.appendChild(eButton);
+    buttonsContainer.appendChild(oButton);
+
+    // Sets text content
+    allButton.textContent = "All"
+    lButton.textContent = "Listings";
+    eButton.textContent = "Events";
+    oButton.textContent = "Offers";
+
+    // Event Listeners
+    allButton.addEventListener('click', () => {
+        // console.log('clicked all listings');
+        filterer('clear', allEntries)
+    })
+
+    lButton.addEventListener('click', () => {
+        // console.log('clicked the listing');
+        filterer("clear", allEntries).then(res => filterer("listings", allEntries))
+
+    })
+
+    eButton.addEventListener('click', () => {
+        // console.log('clicked the events');
+        filterer("clear", allEntries).then(res => filterer("events", allEntries))
+    })
+
+    oButton.addEventListener('click', () => {
+        // console.log('clicked the offers');
+        filterer("clear", allEntries).then(res => filterer("offers"));
+    })
+}
+
+createNavButtons();
+
+function createListings(title, description, image) {
+    const listing = document.createElement('div');
+    const listingImage = document.createElement('img');
+    const textContainer = document.createElement('div');
+    const imageContainer = document.createElement('div');
+    const listingTitle = document.createElement('h2');
+    const listingDescription = document.createElement('p');
+    const listingButton = document.createElement('button');
+
+    // Sets class names
+    listing.classList.add('listing');
+    listingButton.classList.add('expand-button');
+    textContainer.classList.add('text-div');
+    imageContainer.classList.add('image-div');
+
+    // Sets up structure
+    listing.appendChild(imageContainer);
+    imageContainer.appendChild(listingImage);
+    listing.appendChild(textContainer);
+    textContainer.appendChild(listingTitle);
+    textContainer.appendChild(listingDescription);
+    listing.appendChild(listingButton);
+
+    // Sets text content
+    listingImage.src = image;
+    listingTitle.textContent = title;
+    listingDescription.textContent = description;
+    listingButton.textContent = 'More Info';
+
+    // Event Listeners
+    listing.addEventListener('mouseover', () => {
+        // console.log('mouse entered');
+        listingButton.classList.toggle('more-info');
+
+    })
+
+    listing.addEventListener('mouseout', () => {
+        // console.log('mouse left');
+        listingButton.classList.remove('more-info');
+    })
+
+    return listing;
+
+
+}
