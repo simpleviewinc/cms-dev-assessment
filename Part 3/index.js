e0184c189c3ef5eb4adb2c6810867a1f6b62e37b
Victoria Chen <vicchen12@gmail.com>
2021-02-18 13:03:30 -0800

    create functions to develop the grid cols/rows, fill slides with api data

--- Part 3/index.js
+++ Part 3/index.js
@@ -1,16 +1,54 @@
 'use strict';
 
 (function() {
-  const buttons = document.querySelectorAll('button')
+  const buttons = document.querySelectorAll('button');
+  const slideOne = document.querySelector('.slide-one');
+  const slideTwo = document.querySelector('.slide-two');
+  const slideThree = document.querySelector('.slide-three');
+  const slideFour = document.querySelector('.slide-four');
 
+  fetch('https://sv-reqres.now.sh/api/listings?per_page=20')
+    .then(res => {
+      return res.json()
+    }).then(data => {
+      slideOne.innerHTML = createGrid(2, [2, 3], data.data)
+      slideTwo.innerHTML = createGrid(3, [2, 2, 1], data.data)
+      slideThree.innerHTML = createGrid(2, [2, 1], data.data)
+      slideFour.innerHTML = createGrid(2, [1, 1], data.data)
+    }).catch(err => {
+      console.error(err);
+    })
+
+  //create grid by inputting number of columns
+  //rowsPerColArr is an array of numbers, each number represents amount of rows per column given as first argument
+  const createGrid = function(numOfCols, rowsPerColArr, data) {
+    let rowsInsideCol = ''
+    rowsPerColArr.forEach(num => {
+      //need to randomize data in part 4
+      rowsInsideCol += createRowsInsideCol(num, data)
+    })
+    return `<div class="grid-container-${numOfCols}col">${rowsInsideCol}</div>`
+  }
+
+  const createRowsInsideCol = function(numOfRows, data) {
+    let gridItem = ''
+    for(let i = 0; i < numOfRows; i++) {
+      if(data.length > 0) {
+        gridItem += `<div class="grid-item"><h2>${data[0].title}</h2><img src=${data[0].mediaurl} onerror="this.onerror=null;this.src='../comps/fallback.jpg'" alt="farmland"></div>`
+        data.shift()
+      }
+    }
+    return `<div class="grid-container-${numOfRows}row">${gridItem}</div>`
+  }
+
+//Slider
   let slideIndex = 1;
   showSlides(slideIndex);
-
   // next/previous controls
   function plusSlides(n) {
     showSlides(slideIndex += n);
   }
-
+  // change display of slides
   function showSlides(n) {
     var slides = document.querySelectorAll('.slide');
     if (n > slides.length) {slideIndex = 1}
@@ -20,7 +58,7 @@
     }
     slides[slideIndex-1].style.display = 'block';
   }
-
+  //add event to prev/next buttons
   buttons.forEach(button => {
     button.addEventListener('click', (event) => {
       if(event.target.id === 'prev') {
