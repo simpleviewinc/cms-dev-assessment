7018281ed653c6d43013eb4c2b35edca5cfe6592
David Harris <jdh.programmer@gmail.com>
2021-03-05 00:42:30 -0600

    finalized part1

--- JDH_Solutions/part-1/src/utils/API.js
+++ JDH_Solutions/part-1/src/utils/API.js
@@ -7,13 +7,14 @@ const OFFERS = "/api/offers";
 const API = {
   listings: function () {
     console.log('getting listings')
-    console.log(BASEURL + LISTINGS)
     return axios.get(BASEURL + LISTINGS);
   },
   events: function () {
+    console.log('getting events')
     return axios.get(BASEURL + EVENTS);
   },
   offers: function () {
+    console.log('getting offers')
     return axios.get(BASEURL + OFFERS);
   }
 };
